b333dc78d8c33ebf26909ebfed84087ab6156c1d
Gaetan <gaetan@protonmail.com>
2019-10-08 15:52:39 +0200

    add a style into the nabbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="Navbar">
+      <a href="https://en.wikipedia.org/wiki/The_Simpsons">Simpons Wiki</a>
+      <br/> <p className="kiki"> It's A Nice App !!! </p>
+    </nav>
+  );
+}
+
+export default Navbar;

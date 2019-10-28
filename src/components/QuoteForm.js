be754c12fd609915732bf799d5fe9ceb56926678
Tim <Tim@MacBook-Pro-de-Tim.local>
2019-10-28 12:05:06 +0100

    resolve ex with function of input

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import  "./QuoteForm.css"
 
+const MAX_LENGTH = 30; // You can change the value
 
 class QuoteForm extends React.Component {
     constructor(props){
@@ -11,8 +12,9 @@ class QuoteForm extends React.Component {
     }
 
     handleChange =(event) => {
-        this.setState({character: event.target.value})
-        
+        if (event.target.value.length > MAX_LENGTH)
+            return;
+        this.setState({character: event.target.value})   
     }
 
     handleSubmit(event) {
@@ -20,19 +22,27 @@ class QuoteForm extends React.Component {
       }
     
 
+
     render() {
+
+    const maximumReached = this.state.character.length >= MAX_LENGTH;
+    const numRemaining = MAX_LENGTH - this.state.character.length;
       return (
         <form
           className="QuoteForm"
         >
           <label htmlFor="character">Character:</label>
           <input
+          className={maximumReached ? 'length-maximum-reached' : 'length-ok'}
             id="name"
             name="character"
             type="text"
             value={this.state.character}
             onChange={this.handleChange}
           />
+          <small className="remaining-characters">
+          {numRemaining} remaining characters
+        </small>
           <p><strong>You typed: {this.state.character}</strong> </p>
         </form>
       );
